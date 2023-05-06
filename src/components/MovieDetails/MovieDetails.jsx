3f5900f701abd7c5265c5f63aab99ddb35f73270
Iceonq <bartek.sosnowicz@gmail.com>
2023-05-06 07:14:50 +0200

    .

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -6,7 +6,8 @@ import { fetchMovieDetails } from '../../utils/fetchMovieDetails';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(
     () => async () => {
@@ -14,39 +15,43 @@ const MovieDetails = () => {
       if (movieData) {
         setMovie(movieData);
       }
+      setLoading(false);
     },
-    [movieId]
+    [movieId, loading]
   );
 
-  if (!movie) {
-    return null;
-  }
-  const averageScore = Math.ceil(movie.vote_average * 10);
-
   return (
-    <div>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path} `}
-        alt="cze"
-      />
-      <Link to="/">Go back</Link>
-      <h2>Title: {movie.title}</h2>
-      <p>User Score: {averageScore}%</p>
-      <h3>Overview</h3>
-      <p>{movie.overview}</p>
-      <h3>Genres</h3>
+    <>
+      {loading ? (
+        <div>Loading... </div>
+      ) : movie ? (
+        <div>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path} `}
+            alt="cze"
+          />
+          <Link to="/">Go back</Link>
+          <h2>Title: {movie.title}</h2>
+          <p>User Score: {Math.ceil(movie.vote_average * 10)}%</p>
+          <h3>Overview</h3>
+          <p>{movie.overview}</p>
+          <h3>Genres</h3>
 
-      <ul>
-        {movie.genres &&
-          movie.genres.map(movieGenre => {
-            return <li key={nanoid()}>{movieGenre.name}</li>;
-          })}
-      </ul>
-      <span>Additional information</span>
-      <Link to={`cast`}>Cast</Link>
-      <Link to={`reviews`}>Reviews</Link>
-      <Outlet />
-    </div>
+          <ul>
+            {movie.genres &&
+              movie.genres.map(movieGenre => {
+                return <li key={nanoid()}>{movieGenre.name}</li>;
+              })}
+          </ul>
+          <span>Additional information</span>
+          <Link to={`cast`}>Cast</Link>
+          <Link to={`reviews`}>Reviews</Link>
+          <Outlet />
+        </div>
+      ) : (
+        <div>There is no movie with this id!</div>
+      )}
+    </>
   );
 };
 
